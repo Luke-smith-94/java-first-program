33ad0f61509e0726b2797b25f46bf2d4c2b5e47d
lusmith <luke.smith@autogeneral.com.au>
2021-05-25 09:51:01 +1000

    luke

---
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -0,0 +1,30 @@
+package com.h2;
+
+public class MortgageCalculator {
+
+    private long loanAmount;
+
+    private int termInYears;
+
+    private float annualRate;
+
+    private double monthlyPayment;
+
+    public MortgageCalculator (long loanAmount, int termInYears, float annualRate) {
+        this.loanAmount = loanAmount;
+        this.termInYears = termInYears;
+        this.annualRate = annualRate;
+    }
+
+    private int getNumberOfPayments() {
+        return termInYears * 12;
+    }
+
+    private float getMonthlyInterestrate() {
+        float interestRate = annualRate / 12;
+        return interestRate;
+    }
+
+
+
+}
