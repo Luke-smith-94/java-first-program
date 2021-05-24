6c200cd6b4aa47adec22e68e00b16eb86e850ca1
lusmith <luke.smith@autogeneral.com.au>
2021-05-24 15:07:31 +1000

    luke

---
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -0,0 +1,18 @@
+package com.h2;
+
+public class SavingsCalculator {
+    private float[] credits;
+    private float[] debits;
+
+    public SavingsCalculator(float[] credits, float[] debits) {
+        this.credits = credits;
+        this.debits = debits;
+    }
+    private float sumOfCredits() {
+        float sum = 0.0f;
+        for (float credit : credits) {
+            sum += credit;
+        }
+        return sum;
+    }
+}
