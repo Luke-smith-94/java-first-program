18ee391784d81f887e06a136a70daf2d4ef683fe
lusmith <luke.smith@autogeneral.com.au>
2021-05-24 11:20:37 +1000

    luke

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -1,13 +1,33 @@
 package com.h2;
 
+import java.util.Map;
 import java.util.Scanner;
 
 public class BestLoanRates {
-}
+    public static final Map <Integer, Float> bestRates = Map.of(1, 5.50f, 2, 3.45f, 3, 2.67f);
 
-    public static void main( String[] args ) {
+
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name");
         String name = scanner.nextLine();
         System.out.println("Hello " + name);
+        System.out.println("Enter the loan term (in years)");
+        int loanTermInYears = scanner.nextInt();
+        float bestRates = getRates(loanTermInYears);
+        if (bestRates == 0.0f) {
+            System.out.println("No available rates for term: " + loanTermInYears + "years");
+        } else {
+            System.out.println("best Available Rate: " + bestRates + "%");
+
+        }
     }
+
+    public static float getRates(int loanTermInYears) {
+        if (bestRates.containsKey(loanTermInYears)) {
+            return bestRates.get(loanTermInYears);
+
+        }
+        return 0.0f;
+    }
+}
